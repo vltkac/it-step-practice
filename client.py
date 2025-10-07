b5db32dcf269f6b401f1d9a4c5bb9d591aaf615a
vltkac <vladtkachuk135@gmail.com>
2025-10-07 13:18:44 +0200

    hw file added

--- client.py
+++ client.py
@@ -1,117 +1,67 @@
-# # import requests
-# #
-# #
-# # response = requests.post('http://localhost:8000/message')
-# # print(response.status_code)
-# #
-# # response1 = requests.post('http://localhost:8000/function')
-# # print(response1.status_code)
-# #
-# # response2 = requests.post('http://localhost:8000/data')
-# #
-# # if response2.ok:
-# #     data = response2.json()
-# #     print(data)
-# # else:
-# #     print(response2.text)
-# #
-# # num = 24
-# # response = requests.post(f"http://localhost:8000/mult2/{num}")
-# #
-# # if response.ok:
-# #     data = response.json()
-# #     print(data)
-# # else:
-# #     print(response.text)
-#
-# import requests
-#
-#
-# response = requests.post("http://localhost:8000/hello/vlad")
-#
-# if response.ok:
-#     data = response.json()
-# else:
-#     print(response.text)
-#
-#
-# name_data = {
-#     "name": "vlad"
-# }
-#
-# response = requests.post("http://localhost:8000/hello_json")
-#
-# if response.ok:
-#     data = response.json()
-# else:
-#     print(response.text)
-#
-#
-#
-
-
 import requests
 
 
-while True:
-    user_choice = input('Choose option\nTo get all books press (1)\nTo add new book press (2) ').strip()
+def get_movie_data(movie_id: int):
+    response = requests.get(f'http://localhost:8000/movies/{movie_id}')
 
-    if user_choice == '1':
-        response = requests.get('http://localhost:8000/books')
-
-        if response.ok:
-            data = response.json()
-
-            print(data)
-        else:
-            print(response.text)
-
-    elif user_choice == '2':
-        book_id = int(input('Please enter book\'s id: ').strip())
-        book_title = input('Please enter book\'s title: ').strip()
-        book_author = input('Please enter book\'s author: ').strip()
-        book_year = int(input('Please enter book\'s year: ').strip())
-        book_pages = int(input('Please enter book\'s pages quantity: ').strip())
-
-        new_book = {
-            "id": book_id,
-            "title": book_title,
-            "author": book_author,
-            "year": book_year,
-            "pages": book_pages
-        }
-
-        response = requests.post('http://localhost:8000/books', json=new_book)
-
-        if response.ok:
-            print('Book is added')
-        else:
-            print(response.text)
+    if response.ok:
+        print(response.json())
+    else:
+        print(response.text)
 
 
+def post_new_movie(title: str, director: str, year: int):
+    id_response = requests.get('http://localhost:8000/new_id')
+
+    if not id_response.ok:
+        print(id_response.text)
+        return
+
+    new_id = id_response.json()
+
+    new_movie = {
+        'id': new_id,
+        'title': title,
+        'director': director,
+        'year': year
+    }
+
+    response = requests.post('http://localhost:8000/movies', json=new_movie)
+
+    if response.ok:
+        print('New movie was added')
+    else:
+        print(response.text)
 
 
+def delete_movie(movie_id: int):
+    response = requests.delete(f"http://localhost:8000/movies/{movie_id}")
+
+    if response.ok:
+        print('Movie was deleted')
+    else:
+        print(response.text)
 
 
+def main():
+    while True:
+        user_option = input('To display movie data by its id click (1)\nTo add new movie click (2)\nTo delete movie click (3)\nTo stop client click (4)\n').strip()
+
+        if user_option == '1':
+            search_id = int(input('Please enter valid movie id: ').strip())
+            get_movie_data(search_id)
+        elif user_option == '2':
+            new_title = input('Please enter the title: ').strip()
+            new_director = input('Please enter the director: ').strip()
+            new_year = int(input('Please enter the year of production: ').strip())
+            post_new_movie(new_title, new_director, new_year)
+        elif user_option == '3':
+            delete_id = int(input('Please enter valid movie id: ').strip())
+            delete_movie(delete_id)
+        elif user_option == '4':
+            print('Thank you for using!')
+            break
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,127 +1,75 @@
-# from fastapi import FastAPI
-#
-#
-# app = FastAPI()
-#
-#
-# @app.post('/message')
-# def message():
-#     print('message function calling')
-#
-#
-# @app.post('/function')
-# def func():
-#     print('func function calling')
-#
-#
-# @app.post('/data')
-# def get_data():
-#     return {"result": "Hello from server"}
-#
-#
-# @app.post("/mult2/{num}")
-# def mult2(num: int):
-#     result = 2 * num
-#     return {'result': result}
-
-
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-#
-#
-# app = FastAPI()
-#
-# @app.post("/hello/{name}")
-# def hello(name: str):
-#     return {"message": f"Hello, {name}!"}
-#
-#
-# class HelloName(BaseModel):
-#     name: str
-#
-#
-# @app.post("/hello_json")
-# def hello_json(hello_name: HelloName):
-#     name = hello_name.name
-#     return {"message": f"Hello, {name}!"}
-
-# Завдання 4
-# Напишіть сервер для симуляції роботи бібліотеки.
-# Дані про книги знаходяться у файлі books.json
-# Напишіть модель на pydentic для книги з такими
-# даними:
-# ● id
-# ● title
-# ● author
-# ● year
-# ● pages
-# Функціонал:
-# 1. Отримання всіх книг
-# ○ шлях – books
-# ○ метод – GET
-# 2. Отримання даних за ID книги
-# ○ шлях – books/{book_id}
-# ○ метод – GET
-# 3. Додавання нової книги
-# ○ шлях – books
-# ○ метод – POST
-# 4. Видалення книги за ID
-# ○ шлях – books/{book_id}
-# ○ метод – DELETE
-
-from fastapi import FastAPI
-from pydantic import BaseModel
 import json
+import os
+from pydantic import BaseModel
+from fastapi import FastAPI
 
 
-class Book(BaseModel):
+class Movie(BaseModel):
     id: int
     title: str
-    author: str
+    director: str
     year: int
-    pages: int
+
 
 app = FastAPI()
 
-@app.get("/books")
-def get_all_books(filename='books.json'):
-    with open(filename, 'r', encoding='utf-8') as f:
+@app.get("/movies/{movie_id}")
+def get_movie_data(movie_id: int):
+    try:
+        with open('films.json', 'r', encoding='utf-8') as f:
+            source_data = json.load(f)
+
+        return source_data.get(str(movie_id))
+
+    except FileNotFoundError:
+        print('No file films.json in the current working directory')
+        return None
+
+
+@app.get("/new_id")
+def get_movie_new_id():
+    if not os.path.exists('get_movie_new_id.json'):
+        with open('get_movie_new_id.json', 'w') as f:
+            json.dump([0], f)
+
+    with open('get_movie_new_id.json', 'r') as f:
+        id_last = json.load(f)
+        current_id = id_last[0] + 1
+
+    with open('get_movie_new_id.json', 'w') as f:
+        json.dump([current_id], f)
+
+    return current_id
+
+
+@app.post("/movies")
+def post_new_movie(movie: Movie):
+    if not os.path.exists('films.json'):
+        with open('films.json', 'w', encoding='utf-8') as f:
+            json.dump({}, f)
+
+    with open('films.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    return data
+    data[str(movie.id)] = movie.dict()
+
+    with open('films.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-@app.post("/books")
-def add_new(book_json: Book, filename='books.json'):
-    with open(filename, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+@app.delete("/movies/{movie_id}")
+def delete_movie(movie_id: int):
+    try:
+        with open('films.json', 'r', encoding='utf-8') as f:
+            source_data = json.load(f)
 
-    data[book_json.id] = book_json.dict()
+        try:
+            source_data.pop(str(movie_id))
+            with open('films.json', 'w', encoding='utf-8') as f:
+                json.dump(source_data, f, ensure_ascii=False, indent=4)
 
-    with open(filename, 'w') as f:
-        json.dump(data, f, indent=4)
+        except KeyError:
+            print('Searched movie id was not found')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    except FileNotFoundError:
+        print('No file films.json found in the current working directory')
